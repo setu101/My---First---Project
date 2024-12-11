6a4db311d1feb04672d8be0c2a714673b62a4d06
setu101 <setumallick996@gmail.com>
2024-12-10 20:56:18 -0500

    new update

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello GitHub! This is another update");
+        System.out.println("Checking another sentence for github");
 
     }
 }
