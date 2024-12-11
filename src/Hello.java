678d4ccac42c324088ee4fb6975c8a6c8d915fac
setu101 <setumallick996@gmail.com>
2024-12-10 21:15:52 -0500

    added basic for loop

--- src/Hello.java
+++ src/Hello.java
@@ -3,5 +3,12 @@ public class Hello {
         System.out.println("Hello GitHub! This is another update");
         System.out.println("Checking another sentence for github");
 
+
+    // This is just a basic for loop
+        for (int i=0; i <10; i++){
+            System.out.println(i);
+
+        }
+
     }
 }
